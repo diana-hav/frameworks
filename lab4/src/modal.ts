4d3f42d945c53405f4d66b7be47905987bd79163
diana-hav <dgavrish1205@gmail.com>
2024-10-01 01:05:12 +0300

    add files for lab4

---
+++ lab4/src/modal.ts
@@ -0,0 +1,35 @@
+
+import * as bootstrap from 'bootstrap';
+
+export class ModalManager{
+    private modalElement: HTMLElement | null = null;
+
+    constructor(){}
+
+    public open(modalId: string) : void{
+            this.modalElement = document.getElementById(modalId);
+            if(this.modalElement){
+                const modal = new bootstrap.Modal(this.modalElement);
+                modal.show();
+            } else{
+                console.error(`modal with id ${modalId} not found`)
+            }
+    }
+
+    public close(modalId: string): void{
+        this.modalElement = document.getElementById(modalId);
+        if (this.modalElement){
+            const modal = bootstrap.Modal.getInstance(this.modalElement);
+            if(modal){
+                modal.hide();
+            }
+        }
+    }
+
+    public setModalContent(modalId: string, content: string): void{
+        const modalBody = document.querySelector(`#${modalId} .modal-body`) as HTMLElement;
+        if (modalBody){
+            modalBody.innerHTML = content;
+        }
+    }
+}
