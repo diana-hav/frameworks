89320e06f00fb71f0a69958b96ccca199006fb4c
diana-hav <dgavrish1205@gmail.com>
2024-10-01 10:16:40 +0300

    first test commit

--- lab4/eslint.config.mjs
+++ lab4/eslint.config.mjs
@@ -3,26 +3,31 @@ import pluginJs from "@eslint/js";
 import tseslint from "@typescript-eslint/eslint-plugin"; 
 import tsParser from "@typescript-eslint/parser";
 
-export default [
-  {
-    files: ["**/*.{js,mjs,cjs,ts}"],
-    languageOptions: {
-      globals: {
-        ...globals.browser,
-        ...globals.node,
-      },
-      parser: tsParser, 
-      parserOptions: {
-        ecmaVersion: 2021, 
-        sourceType: "module",
-      },
+const tsConfig = {
+  files: ["**/*.{js,mjs,cjs,ts}"],
+  languageOptions: {
+    globals: {
+      ...globals.browser,
+      ...globals.node,
+    },
+    parser: tsParser,
+    parserOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      project: ["tsconfig.json"], // Вказати файл конфігурації TypeScript
     },
   },
+  rules: {
+    "prettier/prettier": "error",
+  },
+};
+
+const configs = [
   pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  {
-    rules: {
-      "prettier/prettier": "error", 
-    },
-  },
+  tseslint.configs.recommended,
+];
+
+export default [
+  tsConfig,
+  ...configs,
 ];
